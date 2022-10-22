0a4a32301bf96feff2b415fa4cec30d5c7d111c1
amunoz <soporte@type.cl>
2022-10-22 16:04:12 -0300

    Initial Commit

---
+++ turshop/turshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  turshopApp.swift
+//  turshop
+//
+//  Created by Paula Torres on 22-10-22.
+//
+
+import SwiftUI
+
+@main
+struct turshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
